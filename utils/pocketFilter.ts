e5dfe5c441835776cd8729cb8e6783dc3f723983
jw <jacekwoln@gmail.com>
2025-02-07 14:54:57 +0100

    add dockers

--- utils/pocketFilter.ts
+++ utils/pocketFilter.ts
@@ -1,14 +1,33 @@
 export class PocketFilter {
     eq(name: string | object, value?: any) {
-        return this.getExpression(name, "=", value);
+        return this.getExpression(name, "?=", value);
     }
 
     getExpression(object: string | object, operator: string, value: any): string {
         if (typeof object === 'object') {
-            return Object.entries(object)
-                .map(([key, val]) => this.getExpression(key, operator, val))
+            let query = Object.entries(object)
+                .map(([key, val]) => {
+                    return this.getExpression(key, operator, val)
+                })
                 .join(' && ');
+            if (query.endsWith((' && '))) {
+                query = query.replace(' && ', '')
+            }
+            return query
         }
+
+        if (value === undefined)
+            return ''
+
+        if (typeof value === 'object') {
+            let props = Object.entries(value)
+                .map(([key, val]) => {
+                    return this.getExpression(object + "." + key, operator, val)
+                })
+                .join(' && ');
+            return  props
+        }
+
         return `${object}${operator}${typeof value === 'string' ? `"${value}"` : value}`;
     }
 
