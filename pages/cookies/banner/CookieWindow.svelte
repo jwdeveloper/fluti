2070521e3f716fb269936a52b6e12ff17ffc27e3
mremiszewicz <mremiszewicz@wwsystem.com.pl>
2025-03-19 14:57:18 +0100

    .

--- pages/cookies/banner/CookieWindow.svelte
+++ pages/cookies/banner/CookieWindow.svelte
@@ -7,6 +7,7 @@
     import CookieBanner from "$lib/fluti/pages/cookies/banner/CookieBanner.svelte";
     import type {CookiePage} from "$lib/fluti/pages/cookies/cookiePageTypes";
     import {onMount} from "svelte";
+    import Button2 from "$lib/fluti/components/button/Button2.svelte";
 
     interface CookieWindowProps {
         controller?: CookieWindowControllerSvelte
@@ -35,6 +36,11 @@
         controller.openCookiesWindow();
     })
 
+    let showBanner = $state(true);
+
+    function toggleBanner() {
+        showBanner = !showBanner;
+    }
 </script>
 
 
@@ -47,3 +53,24 @@
         bind:visible={controller.isOpen}>
     <CookieBanner {...page} controller={controller}/>
 </SideWindow>
+{#if showBanner}
+<div class="bg-gray-100 py-6 flex flex-col justify-center sm:py-12 z-50">
+
+    <div>
+<!--          x-transition:enter-start="opacity-0 scale-90"-->
+<!--          x-transition:enter="transition duration-200 transform ease"-->
+<!--          x-transition:leave="transition duration-200 transform ease"-->
+<!--          x-transition:leave-end="opacity-0 scale-90"-->
+<!--          class="max-w-screen-lg mx-auto fixed bg-white inset-x-5 p-5 bottom-40 rounded-lg drop-shadow-2xl flex gap-4 flex-wrap md:flex-nowrap text-center md:text-left items-center justify-center md:justify-between">-->
+        <div class="w-full">This website uses cookies to ensure you get the best experience on our website.
+            <a href="#" class="text-indigo-600 whitespace-nowrap  hover:underline">Learn more</a></div>
+        <div class="flex gap-4 items-center flex-shrink-0">
+            <!-- setTimeout is for demo purposes only. Remove it & add to cookies
+                 so that the popup won't appear next time they load. -->
+            <Button2 onClick={toggleBanner}>Decline</Button2>
+            <Button2 onClick={toggleBanner}>Allow Coockies</Button2>
+        </div>
+    </div>
+
+</div>
+{/if}
