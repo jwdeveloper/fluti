e61920fc38eefd054344fe30377dd2f19e40df71
mremiszewicz <mremiszewicz@wwsystem.com.pl>
2025-03-19 15:07:20 +0100

    .

--- pages/cookies/cookieWindowController.svelte.ts
+++ pages/cookies/cookieWindowController.svelte.ts
@@ -3,18 +3,24 @@ import type {CookieCategoryProps} from "$lib/fluti/pages/cookies/cookiePageTypes
 export class CookieWindowControllerSvelte {
 
     isOpen: boolean = $state(false)
+    isOpenBanner: boolean = $state(true)
 
     openCookiesWindow() {
         if (localStorage.getItem("cookie-window-accepted")) {
             this.isOpen = false;
+            this.isOpenBanner = false;
             return
         }
-        this.isOpen = true;
     }
 
     handleSaveClick(data: CookieCategoryProps[]) {
+
         localStorage.setItem("cookie-window-accepted", JSON.stringify(data))
         this.isOpen = false;
+    }
+
+    handleEditCookiesClick() {
+        this.isOpen = true;
     }
 
 }
