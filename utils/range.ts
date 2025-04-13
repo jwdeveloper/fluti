4407c90ecc8687984ffc41f4a98e3cd79555f183
jw <jacekwoln@gmail.com>
2025-04-13 12:39:30 +0200

    init

--- utils/range.ts
+++ utils/range.ts
@@ -1,8 +1,16 @@
+export function random(from: number, to?: number): number {
+    const max = to === undefined ? from : to;
+    const offset = to === undefined ? from : 0;
+    const number = Math.floor(offset + Math.random() * max)
+    return number;
+}
+
+
 export function range(start: number, end?: number): number[] {
     if (end === undefined) {
         end = start - 1;
         start = 0;
     }
 
-    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    return Array.from({length: end - start + 1}, (_, i) => start + i);
 }
