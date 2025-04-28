370977f5c3f094f7400d2a5170512b5c726db6be
jw <jacekwoln@gmail.com>
2025-04-28 02:33:18 +0200

    init

--- components/containers/Card3D.svelte
+++ components/containers/Card3D.svelte
@@ -4,19 +4,46 @@
     import {onMount} from "svelte";
 
     let {children, isHover = false} = $props();
+
+    let isMouseEntered = $state(false);
+
     let element: HTMLDivElement;
 
+    const handleMouseMove = (e: MouseEvent) => {
+
+        if (!element) return;
+        const {left, top, width, height} = element.getBoundingClientRect();
+        const x = (e.clientX - left - width / 2) / 25;
+        const y = (e.clientY - top - height / 2) / 25;
+        element.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    };
+
+    const handleMouseEnter = (e: MouseEvent) => {
+        isMouseEntered = true;
+        if (!element) return;
+    };
+
+    const handleMouseLeave = (e: MouseEvent) => {
+        if (!element) return;
+        isMouseEntered = false;
+        element.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    };
     onMount(() => {
-        let controller = new Card3DController(element);
+        // let controller = new Card3DController(element);
+        element = undefined;
         return () => {
-            controller.destory();
+            // controller.destory();
         }
     })
 
 </script>
 
 
-<div bind:this={element} class="card-3d-style " class:card-hovered={isHover}>
+<div
+        onmouseenter={handleMouseEnter}
+        onmousemove={handleMouseMove}
+        onmouseleave={handleMouseLeave}
+        bind:this={element} class="card-3d-style " class:card-hovered={isHover}>
     {@render children?.()}
 </div>
 
@@ -52,7 +79,8 @@
         height: 700px;
         /*transition: all 200ms ease-in-out;*/
 
-        transform: perspective(3000px) rotateX(8deg) rotateY(-3deg) rotateZ(4deg) scale(0.9);
+        perspective: 1000px;
+        transform: perspective(1000px) rotateX(8deg) rotateY(-3deg) rotateZ(4deg) scale(0.9);
         filter: blur(0.05em);
         /*opacity: 0.5;*/
         transition: 0.6s ease-in-out all;
@@ -65,6 +93,7 @@
             /*box-shadow: 0 0 2em 0.1em rgba(0, 0, 0, 0.5);*/
 
         }
+
     }
 
     .card-hovered {
