6a24a128de324a8ab5e466c293a1a0f61f99c027
jw <jacekwoln@gmail.com>
2025-04-28 02:16:16 +0200

    init

--- components/containers/Card3D.svelte
+++ components/containers/Card3D.svelte
@@ -55,12 +55,11 @@
         transform: perspective(3000px) rotateX(8deg) rotateY(-3deg) rotateZ(4deg) scale(0.9);
         filter: blur(0.05em);
         /*opacity: 0.5;*/
-        transition: 0.6s ease all;
+        transition: 0.6s ease-in-out all;
 
         &:hover {
             transform: perspective(3000px) rotateX(4deg) rotateY(-0deg) rotateZ(0deg) scale(0.95);
 
-        /*transform: perspective(6000px) rotateX(8deg) rotateY(-180deg) rotateZ(4deg) scale(0.9);*/
             filter: blur(0);
             opacity: 1;
             /*box-shadow: 0 0 2em 0.1em rgba(0, 0, 0, 0.5);*/
