8acdb42019470b6e9f6b1c92792f64861779dea1
jw <jacekwoln@gmail.com>
2025-05-03 19:50:19 +0200

    init

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,3 +1,15 @@
+
+
+export function randomColor():string
+{
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 export function hexToHue(hex: string): number {
     // Remove leading #
     hex = hex.replace(/^#/, '');
