082a500aab265422f3743d695201b18eb2607a07
jw <jacekwoln@gmail.com>
2025-07-13 15:11:21 +0200

    - making users

--- clients/pocketbase-client.ts
+++ clients/pocketbase-client.ts
@@ -2,6 +2,8 @@ import PocketBase from 'pocketbase';
 
 //TODO check it if variables will works in this way
 function getUrl() {
+
+
     const env = process.env.PUBLIC_ENV;
     const localUrl = process.env.PUBLIC_LOCAL_POCKETBASE_URL;
     const prodUrl = process.env.PUBLIC_POCKETBASE_URL;
