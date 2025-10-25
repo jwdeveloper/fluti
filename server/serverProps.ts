2fc7e6f942e03e8fdd12a5bc13a658c34b81cf10
jw <jw@jw.com>
2025-10-25 19:14:47 +0200

    - frontend refactor

--- server/serverProps.ts
+++ server/serverProps.ts
@@ -2,19 +2,24 @@ import { useServerRenderConfig } from "$lib/fluti/components/panel/ServerRenderC
 import type { RequestEvent } from "@sveltejs/kit";
 
 export function returnServerProps(event: RequestEvent) {
-    const isProd = process.env.NODE_ENV === "production";
     const isUserLogin = event.cookies.get("session_token") !== undefined;
 
     useServerRenderConfig.checkIfMobile(event);
 
+    const isProd = process.env.NODE_ENV === "production";
     let url = event.url.toString();
     let domain = event.url.origin;
 
-    // Enforce HTTPS in production
     if (isProd) {
         url = url.replace(/^http:\/\//, "https://");
         domain = domain.replace(/^http:\/\//, "https://");
     }
+    console.log({
+        url,
+        isUser: isUserLogin,
+        domain,
+        route: event.url.pathname
+    })
 
     return {
         url,
