173efddf107ce85f8be9ee5b2a44d4ee75949ac4
jw <jw@jw.com>
2025-10-26 23:35:58 +0100

    Poprawka braku znalezienia produktów

--- widgets/layout/MobileDesktopLayout.svelte
+++ widgets/layout/MobileDesktopLayout.svelte
@@ -85,8 +85,8 @@
 <Element
         display="grid"
         tag="main"
-        columns='auto'
-        rows='auto 1fr auto'
+        columns={dimentions.columns}
+        rows={dimentions.rows}
         width="100%"
         height="auto"
         style="min-height: 100%"
